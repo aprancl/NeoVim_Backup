524baca61b79586e47898e399d4aa9f0ce120ea6
Anthony Prancl <aprancl@mocs.flsouthern.edu>
2023-07-28 19:48:39 -0400

    setup

---
+++ lua/lualine-config/init.lua
@@ -0,0 +1,30 @@
+require('lualine').setup({
+    options = {
+        section_separators = '',
+        component_separators = '|',
+        icons_enabled = false,
+        theme = 'nord'
+        
+    },
+    sections = {
+        -- left
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+
+        -- right
+        lualine_x = { 'g:zoom#statustext', 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+    extensions = { 'quickfix', 'fugitive', 'fzf' },
+})
+--
+--    opts = {
+--      options = {
+--        icons_enabled = false,
+--        theme = 'nord',
+--        component_separators = '|',
+--        section_separators = '',
+--      },
+--    },
